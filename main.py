2c5fdd074e07508eabf4df67da2c8d6120d1304d
hiepph <phanhuyhiep2k3@gmail.com>
2024-09-05 10:30:18 +0700

    update build

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from routes.routerCategory import router_category
 from routes.routerProduct import router_product
 from routes.routerAuth import router_auth
 from routes.routerCart import router_cart
+import uvicorn
 
 app = FastAPI()
 
@@ -12,4 +13,8 @@ app.include_router(router_product)
 app.include_router(router_auth)
 app.include_router(router_cart)
 app.include_router(router_order)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
